47c4e12f0c92e762adaf150cca016eccd266b7c5
Arthur DESMARAIS <adesmarais@amiltone.com>
2020-03-24 15:41:25 +0100

    initialisation de master

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectMohan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
