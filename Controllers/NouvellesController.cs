0ad2bb0fbb9ce334823737e3f515765973bed59a
Arthur DESMARAIS <artdems@hotmail.fr>
2020-03-26 00:36:12 +0100

    teste lecture pdf

--- Controllers/NouvellesController.cs
+++ Controllers/NouvellesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectMohan.BD;
@@ -31,9 +32,16 @@ namespace ProjectMohan.Controllers
         // GET: Nouvelle/Create
         public ActionResult Create()
         {
-            CreateEditeNouvelleViewModel viewmodel = new CreateEditeNouvelleViewModel();
-            viewmodel.Action = "Create";
-            return View("CreateEditeNouvelle",viewmodel);
+            //CreateEditeNouvelleViewModel viewmodel = new CreateEditeNouvelleViewModel();
+            //viewmodel.Action = "Create";
+            //return View("CreateEditeNouvelle",viewmodel);
+            string embed = "<object data=\"{0}\" type=\"application/pdf\" width=\"500px\" height=\"300px\">";
+            embed += "If you are unable to view file, you can download from <a href = \"{0}\">here</a>";
+            embed += " or download <a target = \"_blank\" href = \"http://get.adobe.com/reader/\">Adobe PDF Reader</a> to view the file.";
+            embed += "</object>";
+            TempData["Embed"] = string.Format(embed, "~/pdf/test.pdf");
+
+            return View("CreateEditeNouvelle");
         }
 
         // POST: Nouvelle/Create
